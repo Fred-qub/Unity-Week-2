ee9af499df748ec9e4b477f61b15757e1ba1f6d6
Fred-qub <fwilliamson01@qub.ac.uk>
2025-09-26 20:06:41 +0100

    Tutorial 2.1 complete

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10f;
+    public float xRange = 10f;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+    }
+}
